e5d81974f5b9f0d1bce56bcfb89585fe7224c2df
Dryices <87692678+Dryices@users.noreply.github.com>
2022-01-13 15:32:41 +0800

    Profile page improvement with scrollview

--- app/src/main/java/com/sp/bookshare/ProfileFragment.java
+++ app/src/main/java/com/sp/bookshare/ProfileFragment.java
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
