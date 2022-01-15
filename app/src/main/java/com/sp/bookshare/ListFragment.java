2d2aa2a51b748592b22e8c6f09d4a49e25fe5666
Dryices <87692678+Dryices@users.noreply.github.com>
2022-01-15 17:44:29 +0800

    Add multiple images

--- app/src/main/java/com/sp/bookshare/ListFragment.java
+++ app/src/main/java/com/sp/bookshare/ListFragment.java
@@ -1,5 +1,9 @@
 package com.sp.bookshare;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,12 +14,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class ListFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -27,8 +41,19 @@ public class ListFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    Button select, previous, next;
+    ImageSwitcher imageView;
+    int PICK_IMAGE_MULTIPLE = 1;
+    String imageEncoded;
+    TextView total;
+    ArrayList<Uri> mArrayUri;
+    int position = 0;
+    List<String> imagesEncodedList;
+
     public ListFragment() {
         // Required empty public constructor
+
+
     }
 
     /**
@@ -64,10 +89,104 @@ public class ListFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false);
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
 
+        select = view.findViewById(R.id.select);
+        total = view.findViewById(R.id.text);
+        imageView = view.findViewById(R.id.image);
+        previous = view.findViewById(R.id.previous);
+        mArrayUri = new ArrayList<Uri>();
+
+        // showing all images in imageswitcher
+        imageView.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView1 = new ImageView(getActivity().getApplicationContext());
+                return imageView1;
+            }
+        });
+        next = view.findViewById(R.id.next);
+
+        // click here to select next image
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (position < mArrayUri.size() - 1) {
+                    // increase the position by 1
+                    position++;
+                    imageView.setImageURI(mArrayUri.get(position));
+                } else {
+                    Toast.makeText(getActivity(), "Last Image Already Shown", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // click here to view previous image
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (position > 0) {
+                    // decrease the position by 1
+                    position--;
+                    imageView.setImageURI(mArrayUri.get(position));
+                }
+            }
+        });
+
+        imageView = view.findViewById(R.id.image);
+
+        // click here to select image
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // initialising intent
+                Intent intent = new Intent();
+
+                // setting type to select to be image
+                intent.setType("image/*");
+
+                // allowing multiple image to be selected
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_MULTIPLE);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // When an Image is picked
+        if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == Activity.RESULT_OK && null != data) {
+            // Get the Image from data
+            if (data.getClipData() != null) {
+                ClipData mClipData = data.getClipData();
+                int cout = data.getClipData().getItemCount();
+                for (int i = 0; i < cout; i++) {
+                    // adding imageuri in array
+                    Uri imageurl = data.getClipData().getItemAt(i).getUri();
+                    mArrayUri.add(imageurl);
+                }
+                // setting 1st selected image into image switcher
+                imageView.setImageURI(mArrayUri.get(0));
+                position = 0;
+            } else {
+                Uri imageurl = data.getData();
+                mArrayUri.add(imageurl);
+                imageView.setImageURI(mArrayUri.get(0));
+                position = 0;
+            }
+        } else {
+            // show this if no image is selected
+            Toast.makeText(getActivity(), "You haven't picked Image", Toast.LENGTH_LONG).show();
+        }
     }
 }
+
+
+
